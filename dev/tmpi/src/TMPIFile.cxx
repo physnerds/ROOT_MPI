968f619348f000d2bf540a4efb952e8b4113b8b0
Amit Bashyal <amitbashyal@gmail.com>
2018-07-05 14:09:05 -0500

    Few functions added

--- dev/tmpi/src/TMPIFile.cxx
+++ dev/tmpi/src/TMPIFile.cxx
@@ -11,6 +11,8 @@ TMPIFile is like a TFile except it reads and writes in memory using TMPIFile and
 #include "TROOT.h"
 #include "TArrayC.h"
 #include "TKey.h"
+#include "TClass.h"
+#include "TVirtualMutex.h"
 #include "TMPIFile.h"
 #include "mpi.h"
 /*
@@ -25,12 +27,80 @@ ClassImp(TMPIFile);
 
 TMPIFile::TMPIFile(const char *name, char *buffer, Long64_t size,
 		   Option_t *option,const char *ftitle,Int_t compress,
-		   Int_t np,Int_t split):TMemFile(name,buffer,size,ftitle,compress){
+		   Int_t np,Int_t split):TMemFile(name,buffer,size,option,ftitle,compress){
+  if(buffer)printf("buffer of non 0 size received\n");
   //Initialize MPI's
   MPI_Init(&argc,&argv);
   
 }
-
 TMPIFile::~TMPIFile(){
   MPI_Finalize();
 }
+
+//defining the ParallelFileMerger here.....
+ void TMPIFile::R__MigrateKey(TDirectory *destination, TDirectory *source)
+{
+if (destination==0 || source==0) return;
+TIter nextkey(source->GetListOfKeys());
+   TKey *key;
+   while( (key = (TKey*)nextkey()) ) {
+      TClass *cl = TClass::GetClass(key->GetClassName());
+// printf("R__MigrateKey::Class name so far....%s\n",key->GetClassName());
+      if (cl->InheritsFrom(TDirectory::Class())) {
+         TDirectory *source_subdir = (TDirectory *)source->GetList()->FindObject(key->GetName());
+         if (!source_subdir) {
+            source_subdir = (TDirectory *)key->ReadObj();
+         }
+         TDirectory *destination_subdir = destination->GetDirectory(key->GetName());
+         if (!destination_subdir) {
+            destination_subdir = destination->mkdir(key->GetName());
+         }
+         R__MigrateKey(destination,source);
+      } else {
+         TKey *oldkey = destination->GetKey(key->GetName());
+         if (oldkey) {
+            oldkey->Delete();
+            delete oldkey;
+         }
+         TKey *newkey = new TKey(destination,*key,0 /* pidoffset */); // a priori the file are from the same client ..
+         destination->GetFile()->SumBuffer(newkey->GetObjlen());
+         newkey->WriteFile(0);
+         if (destination->GetFile()->TestBit(TFile::kWriteError)) {
+            return;
+         }
+      }
+   }
+   destination->SaveSelf();
+}
+
+
+void TMPIFile::R__DeleteObject(TDirectory *dir, Bool_t withReset)
+{
+   if (dir==0) return;
+
+ TIter nextkey(dir->GetListOfKeys());
+   TKey *key;
+   while( (key = (TKey*)nextkey()) ) {
+      TClass *cl = TClass::GetClass(key->GetClassName());
+      if (cl->InheritsFrom(TDirectory::Class())) {
+         TDirectory *subdir = (TDirectory *)dir->GetList()->FindObject(key->GetName());
+         if (!subdir) {
+            subdir = (TDirectory *)key->ReadObj();
+         }
+         R__DeleteObject(subdir,withReset);
+      } else {
+         Bool_t todelete = kFALSE;
+         if (withReset) {
+            todelete = (0 != cl->GetResetAfterMerge());
+         } else {
+            todelete = (0 ==  cl->GetResetAfterMerge());
+         }
+         if (todelete) {
+            key->Delete();
+            dir->GetListOfKeys()->Remove(key);
+            delete key;
+         }
+      }
+   }
+}
+
