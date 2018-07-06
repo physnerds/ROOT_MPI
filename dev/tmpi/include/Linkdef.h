64bb32161744f6f1d18d34b6ae7c31694253b265
Amit Bashyal <amitbashyal@gmail.com>
2018-07-06 15:11:47 -0500

    few more changes in linkdef

--- dev/tmpi/include/Linkdef.h
+++ dev/tmpi/include/Linkdef.h
@@ -2,7 +2,9 @@
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
+#pragma link C++ nestedclasses;
 #pragma link C++ class TMPIFile+;
 #pragma link C++ class TClientInfo+;
+
 
 #endif
