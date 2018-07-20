c1cf1c50afce0db181d04afeeda14b587a5d5710
Amit Bashyal <amitbashyal@gmail.com>
2018-07-20 13:27:09 -0500

    some cleanups for theta to run in debug mode

--- dev/tmpi/stable/tools/auto_run.py
+++ dev/tmpi/stable/tools/auto_run.py
@@ -2,14 +2,27 @@ import os,sys,shutil
 import subprocess
 import time
 
-def CheckForLogFile(logfile,cwd):
+def CheckForLogFile(proc,cwd,tot_nodes,tot_ranks,infile):
+    logfile=proc+".output"
+    errorfile=proc+".error"
     files=os.listdir(cwd)
+    if errorfile in files:
+        if os.path.getsize(errorfile)!=0:
+            print "Job had error....Exiting"
+            sys.exit(1)
     if logfile in files:
-        print "DO STH"
+        with open(logfile,'r') as lfile:
+            for line in lfile:
+                #print line
+                if "ElapsedTime" in line:
+                    #print line
+                    newline=line.replace("ElapsedTime= ","")
+                    WriteOutput(tot_nodes,tot_ranks,newline,infile)
+                    
     else:
-        print "No logfiles now sleeping for 5  seconds"
-        time.sleep(5 )
-        CheckForLogFile(logfile,cwd)
+        print "No logfiles now sleeping for 2  minutes"
+        time.sleep(120)
+        CheckForLogFile(proc,cwd,tot_nodes,tot_ranks,infile)
         
 def CreateWrapper(tot_nodes,tot_ranks):
     if (tot_ranks%tot_nodes)!=0:
@@ -20,26 +33,36 @@ def CreateWrapper(tot_nodes,tot_ranks):
     with open("run_template.sh",'rt') as win:
         with open(output_wrapper,"wt") as wout:
             for line in win:
-                print line
+                #print line
                 if "#COBALT -n" in line:
                     wout.write(line.replace("#COBALT -n", "#COBALT -n "+str(tot_nodes)))
                 elif "aprun" in line:
                     wout.write(line.replace("aprun","aprun -n "+str(tot_ranks)+" -N "+str(tot_ranks/tot_nodes)))
                 else:
                     wout.write(line)
-            
+    os.chmod(output_wrapper,0775) #make it executable
+    return output_wrapper
     
-#subprocess.check_output(['source try_time.sh'])
-#subprocess.call(['./try_time.sh'])
-proc=subprocess.check_output(['./try_time.sh'],stderr=subprocess.STDOUT,shell=True)
+def RunExecutableInTheta(tot_nodes,tot_ranks):
+    output=CreateWrapper(tot_nodes,tot_ranks)
+    proc=subprocess.check_output(["qsub",output])
+    proc=proc[:-1] #remove the trailing end of the line output
+    return proc
+
+def WriteOutput(tot_nodes,tot_ranks,est_time,infile):
+    infile.write("tot_nodes="+str(tot_nodes)+" tot_ranks="+str(tot_ranks)+" Est_time="+str(est_time)+"\n")
+    
+
+
+filein=("theta_output_"+str(time.time())+".txt")
+proc=RunExecutableInTheta(8,8)
+infile=open(filein,'w')
+print proc
 proc=proc[:-1] #remove the trailing end of line output
-output=proc+".log"
 print len(proc)
 if proc:
-    print proc,output
+    #print proc,output
     cwd=os.getcwd()
     #CreateWrapper(8,16)
-    logfile=os.getenv("LOGFILE")
-    CheckForLogFile(logfile,cwd)
-
+    CheckForLogFile(proc,cwd,4,8,infile)
 
