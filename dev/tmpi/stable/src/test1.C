34d7e2f44184172ce5ad670e49068fb645d6f430
Amit Bashyal <amitbashyal@gmail.com>
2018-07-31 08:57:33 -0500

    few cleanups and comments

--- dev/tmpi/stable/src/test1.C
+++ dev/tmpi/stable/src/test1.C
@@ -16,14 +16,9 @@
 void test1(){
 TMPIFile *newfile = new TMPIFile("Simple_MPIFile.root","RECREATE");
 int seed = newfile->GetMPILocalSize()+newfile->GetMPIColor()+newfile->GetMPILocalRank();
-//TTree *tree = new TTree("tree","tree");
-// tree->SetAutoFlush(400000000);
-// Float_t px,py;
-// tree->Branch("px",&px);
-// tree->Branch("py",&py);
-// gRandom->SetSeed(seed);
+ int sync_rate = newfile->GetSyncRate();
  //now we need to divide the collector and worker load from here..
- if(newfile->IsCollector())newfile->RunCollector();
+ if(newfile->IsCollector())newfile->RunCollector(); //this one keeps the collector going...
  else{
 TTree *tree = new TTree("tree","tree");
  tree->SetAutoFlush(400000000);
@@ -31,36 +26,34 @@ TTree *tree = new TTree("tree","tree");
  tree->Branch("px",&px);
  tree->Branch("py",&py);
  gRandom->SetSeed(seed);
-   for(int i=0;i<15;i++){
+ //total number of entries
+ Int_t tot_entries = 15;
+   for(int i=0;i<tot_entries;i++){
      //    std::cout<<"Event "<<i<<" local rank "<<newfile->GetMPILocalRank()<<std::endl;
      gRandom->Rannor(px,py);
      int sleep = int(gRandom->Gaus(3,1));
+     //simulating the reco time...per 5 events here....
      if(i%5==0){
        int sleep = int(gRandom->Gaus(3,1));
         std::this_thread::sleep_for(std::chrono::seconds(sleep));
        
      }
       tree->Fill();
-     //at the end of the event loop...put the sync function
+      //at the end of the event loop...put the sync function
+      //************START OF SYNCING IMPLEMENTATION FROM USERS' SIDE**********************
        if(i%(newfile->GetSyncRate())==0){
 	    newfile->Sync(); //this one as a worker...
 	    tree->Reset();
 	      }
    //do the syncing one more time
 	      if(i%(newfile->GetSyncRate())!=0){
-	     if(i==14){
+	     if(i==tot_entries-1){
 	     newfile->Sync(); //to make sure that the final bit also gets written.   
-	     // tree->Reset();
 	      }
 	     }
-   //  printf("Get the size of the file %d\n",newfile->GetSize());
+	      //************END OF SYNCING IMPLEMENTATION FROM USERS' SIDE***********************
 }
  
- //now the clean up part..
- //printf("Clean up part %d\n",newfile->GetMPILocalRank());
-   // MPI_Finalize();
- 
- //here we send empty buffers...put a barrier and so on....
  }
  newfile->MPIClose();
  // newfile->Close();
@@ -73,7 +66,7 @@ int main(int argc,char* argv[]){
   //  printf("start of rank %d\n",rank);
   test1();
   MPI_Finalize();
-   printf("End of test function execution rank %d\n",rank);  
+  //  printf("End of test function execution rank %d\n",rank);  
   return 0;
 }
 #endif
